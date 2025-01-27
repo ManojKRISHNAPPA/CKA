fcfaec75d9548252bd6781d39464c310a6607351
Manoj-K <k_manoj@apple.com>
2025-01-28 00:05:29 +0530

    helm updated

--- HELM/Python-rest-api/main.py
+++ HELM/Python-rest-api/main.py
@@ -1,24 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Sample data for anime list
-anime_list = [
-    {"id": 1, "title": "Fullmetal Alchemist: Brotherhood", "year": 2009, "genre": "Action, Adventure, Fantasy"},
-    {"id": 2, "title": "Attack on Titan", "year": 2013, "genre": "Action, Drama, Fantasy"},
-    {"id": 3, "title": "Naruto: Shippuden", "year": 2007, "genre": "Action, Adventure, Drama"},
-    {"id": 4, "title": "Death Note", "year": 2006, "genre": "Mystery, Thriller, Supernatural"},
-    {"id": 5, "title": "My Hero Academia", "year": 2016, "genre": "Action, Comedy, Superhero"},
-    {"id": 6, "title": "One Piece", "year": 1999, "genre": "Action, Adventure, Comedy"},
-    {"id": 7, "title": "Steins;Gate", "year": 2011, "genre": "Sci-Fi, Thriller"},
-    {"id": 8, "title": "Demon Slayer: Kimetsu no Yaiba", "year": 2019, "genre": "Action, Fantasy, Supernatural"},
-    {"id": 9, "title": "Your Name", "year": 2016, "genre": "Drama, Romance, Supernatural"},
-    {"id": 10, "title": "Cowboy Bebop", "year": 1998, "genre": "Action, Adventure, Sci-Fi"}
-]
 
-@app.route('/')
-def index():
-    return render_template('index.html', anime=anime_list)
+@app.route('/hello', methods=['GET'])
+def helloworld():
+	if(request.method == 'GET'):
+		data = {"data": "Hello World"}
+		return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9001)
